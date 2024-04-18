b2ed0129c6744901aaf962dea89142792390ba57
Maarjani Sanghavi <maarjani23s@gmail.com>
2024-04-18 22:47:47 +0000

    added update functionality

--- app/controllers/api/v1/submitted_content_controller.rb
+++ app/controllers/api/v1/submitted_content_controller.rb
@@ -21,7 +21,19 @@ class Api::V1::SubmittedContentController < ApplicationController
       render json: @submission_record.errors, status: :unprocessable_entity
     end
   end
-  
+
+  def update
+    @submission_record = SubmissionRecord.find(params[:id])
+    if @submission_record.update(submitted_content_params)
+      render json: @submission_record, status: :ok
+    else
+      render json: @submission_record.errors, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  end
+
+
   private
   
   def submitted_content_params
